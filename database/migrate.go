6f5eba1acda19935cc480efc152f598b8e139b95
Ibrahim Al Naz Rana <ranabd36@gmail.com>
2020-04-02 23:27:19 +0600

    Modified migrate.go

--- database/migrate.go
+++ database/migrate.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ranabd36/project-qa/config"
 	"log"
 	"os"
-
+	
 	_ "github.com/lib/pq"
 	"github.com/pressly/goose"
 )
@@ -19,36 +19,111 @@ var (
 )
 
 func main() {
+	flags.Usage = usage
 	_ = flags.Parse(os.Args[1:])
+	
 	args := flags.Args()
-
+	if len(args) == 0 || args[0] == "-h" || args[0] == "--help" {
+		flags.Usage()
+		return
+	}
+	
+	switch args[0] {
+	case "create":
+		if err := goose.Run("create", nil, *dir, args[1:]...); err != nil {
+			log.Fatalf("goose run: %v", err)
+		}
+		return
+	case "fix":
+		if err := goose.Run("fix", nil, *dir); err != nil {
+			log.Fatalf("goose run: %v", err)
+		}
+		return
+	case "-h", "--help":
+		flags.Usage()
+		return
+	}
+	
 	if len(args) < 1 {
 		flags.Usage()
 		return
 	}
-
-	command := args[1]
-
-	dbstring := getDBString()
-
-	if err := goose.SetDialect(conf.Database.DatabaseDriver); err != nil {
-		log.Fatalf("goose: failed to set dialect: %v\n", err)
+	
+	command := args[0]
+	driver := conf.Database.DatabaseDriver
+	dbString := getDBString()
+	
+	switch driver {
+	case "postgres", "mysql", "sqlite3", "redshift":
+		if err := goose.SetDialect(driver); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatalf("%q driver not supported\n", driver)
 	}
-
-	db, err := sql.Open(conf.Database.DatabaseDriver, dbstring)
+	
+	if dbString == "" {
+		log.Fatalf("-dbString=%q not supported\n", dbString)
+	}
+	
+	if driver == "redshift" {
+		driver = "postgres"
+	}
+	
+	db, err := sql.Open(driver, dbString)
 	if err != nil {
-		log.Fatalf("Failed to open DB %v\n", err)
+		log.Fatalf("Invalid DB string:%q %v\n", dbString, err)
 	}
-
+	
 	var arguments []string
-	if len(args) > 2 {
-		arguments = append(arguments, args[2:]...)
+	if len(args) > 1 {
+		arguments = append(arguments, args[1:]...)
 	}
-
+	
 	if err := goose.Run(command, db, *dir, arguments...); err != nil {
-		log.Fatalf("goose %v: %v", command, err)
+		log.Fatalf("goose run: %v", err)
 	}
 }
+
+func usage() {
+	log.Print(usagePrefix)
+	flags.PrintDefaults()
+	log.Print(usageCommands)
+}
+
+var (
+	usagePrefix = `Usage: migrate [OPTIONS] COMMAND
+Drivers:
+    postgres
+    mysql
+    sqlite3
+    redshift
+Examples:
+    goose status
+    goose create init sql
+    goose create add_some_column sql
+    goose create fetch_user_data go
+    goose up
+    goose down
+    goose redo
+Options:
+	-dir string
+		directory with migration files (default ".")
+`
+	
+	usageCommands = `
+Commands:
+    up                   Migrate the DB to the most recent version available
+    up-to VERSION        Migrate the DB to a specific VERSION
+    down                 Roll back the version by 1
+    down-to VERSION      Roll back to a specific VERSION
+    redo                 Re-run the latest migration
+    status               Dump the migration status for the current DB
+    version              Print the current version of the database
+    create NAME [sql|go] Creates new migration file with the current timestamp
+	fix                  Apply sequential ordering to migrations
+`
+)
 
 func getDBString() string {
 	if conf.Database.DatabaseDriver == "mysql" {
